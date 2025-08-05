54d1bed03f4c7a0f9051d3a826956e9150a0cc8d
Abdulmajid <abdulmajidmukhammadjonov3@gmail.com>
2025-08-05 17:05:44 +0500

    Car clasini yozdik

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-users = ['mdfv', 'ofn']
-print(users)
+users = ['Salom', 'Hello']
+print(users[0])
 
+#---------
 
 def say_hello(name: str):
     return f"Assalomu alaykum {name}"
 
 print(say_hello("Abdulmajid"))
+
+#----------
+
+class Car:
+    def __init__(self, model: str, bradn: str, price: int, color:str):
+        self.model = model
+        self.brand = bradn
+        self.price = price
+        self.color = color
+
+    def to_dict(self):
+       return self.__dict__
